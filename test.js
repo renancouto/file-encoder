09b002d891448269068c8f63cfef66e4c62f351e
Renan Couto <renancouto@gmail.com>
2015-05-19 23:01:47 -0300

    initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+/*globals describe, it*/
+'use strict'
+
+/**
+ * dependencies
+ */
+import assert from 'assert'
+
+/**
+ * tests
+ */
+describe('File encoding tests', () => {
+  it('convert the file', () => {
+    assert.equal(true, false)
+  })
+})
